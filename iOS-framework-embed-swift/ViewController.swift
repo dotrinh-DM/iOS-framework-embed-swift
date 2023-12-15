4a3a143c5b7f913ae673a1ffb861b4a369a1988f
dotrinh <>
2023-12-15 20:11:39 +0900

    init

---
+++ iOS-framework-embed-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-framework-embed-swift
+//
+//  Created by dotrinh on 2023/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
